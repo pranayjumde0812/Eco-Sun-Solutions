f7636c6f5989e30d30976bacaa03a72e6881a06b
Pranay Jumde <pranayjumde13@gmail.com>
2024-08-04 23:04:44 +0530

    initial commit

---
+++ Eco-Sun-Solutions-backend/src/main/java/com/ecosun/EcoSunSolutionsApplication.java
@@ -0,0 +1,26 @@
+package com.ecosun;
+
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class EcoSunSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoSunSolutionsApplication.class, args);
+	}
+	
+	@Bean // equivalent to <bean id ..../> in xml file
+	public ModelMapper mapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration()
+		.setMatchingStrategy(MatchingStrategies.STRICT)
+		.setPropertyCondition(Conditions.isNotNull());
+		return modelMapper;
+	}
+
+}
---
+++ Eco-Sun-Solutions-backend/src/test/java/com/suntech/EcoSunSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suntech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoSunSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
