2cfcec4a95473e50f611615337b107e9e5019cc7
Pranay Jumde <pranayjumde13@gmail.com>
2024-08-14 23:59:55 +0530

    start of role based api and authorized api

--- Eco-Sun-Solutions-backend/src/main/java/com/ecosun/controller/UserController.java
+++ Eco-Sun-Solutions-backend/src/main/java/com/ecosun/controller/UserController.java
@@ -6,6 +6,7 @@ import com.ecosun.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping
 	public ResponseEntity<?> getAllUsers() {
 
